70004a0e0b8710f691a1b5385fe65a0e94ac7534
Your Name <your.email@example.com>
2024-02-22 19:25:15 +0100

    new commit

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
 )
---
+++ Client/src/sections/Nav.jsx
@@ -0,0 +1,35 @@
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div className=" grid grid-cols-3 justify-center items-center border-b">
+      <img
+        src="/assets/images/FinWise.png"
+        alt="logo"
+        width={150}
+        height={100}
+        className=" relative top-2 left-20"
+      />
+      <div className=" flex gap-20">
+        <h1 className=" text-xl">Counseling</h1>
+        <h1 className=" text-xl">Recommendation</h1>
+        <h1 className=" text-xl">Learning</h1>
+      </div>
+      <div className=" flex gap-4 ml-24 ">
+        <Link to="/Login">
+          <Button label="Sign in" textColor="text-black" />
+        </Link>
+        <Link to="/Register">
+          <Button
+            label="Get Started"
+            background=" bg-myblue"
+            textColor="text-white"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
